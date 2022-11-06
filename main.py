b16e82e13bd2e1d86f4678b445c7878783900130
touyu <yuuto3039@gmail.com>
2022-11-07 00:38:58 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    url = "https://reservation.anaihghotels.co.jp/booking/stay_pc/rsv/index.aspx?hi_id=41&lang=ja-JP&PMID=99502222"
+    return
+
+if __name__ == "__main__":
+    main()
