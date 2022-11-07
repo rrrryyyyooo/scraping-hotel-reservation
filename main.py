94fbf47d7bad440e7f652374db0d13d56f8c2745
touyu <yuuto3039@gmail.com>
2022-11-08 02:12:27 +0900

    implement check logic

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
 
 def main():
-    url = "https://reservation.anaihghotels.co.jp/booking/stay_pc/rsv/index.aspx?hi_id=41&lang=ja-JP&PMID=99502222"
-    return
+    url = "https://reservation.anaihghotels.co.jp/booking/stay_pc/rsv/index.aspx?hi_id=41&lang=ja-JP&PMID=99502222&dt=2022/12/04"
+    
+    # initialize driver
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    sleep(1)
+
+    # push button
+    driver.find_element_by_xpath('//*[@id="SearchBox_LnkBtnSearch"]').click()
+    sleep(1)
+
+    if driver.find_elements_by_xpath('//*[@id="PNoDataErr"]'):
+        print('Ng')
+    else:
+        print('ok')
 
 if __name__ == "__main__":
     main()
