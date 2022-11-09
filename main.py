8d72b174df8a77c5130e9711c9b1c8c8860ab8d0
touyu <yuuto3039@gmail.com>
2022-11-10 00:53:14 +0900

    add email process

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-import requests
+import os
 import chromedriver_binary
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
 from selenium import webdriver
 from time import sleep
 
+def send_email(date, text):
+    sendAddress = os.environ.get("FROMADDRESS")
+    password = os.environ.get("APP_PASSWORD")
+
+    subject = f'{date}の空き状況'
+    bodyText = text
+    fromAddress = os.environ.get("FROMADDRESS")
+    toAddress = os.environ.get("FROMADDRESS")
+
+    # connect to SMTP server
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.starttls()
+    smtpobj.login(sendAddress, password)
+
+    # create email
+    msg = MIMEText(bodyText)
+    msg['Subject'] = subject
+    msg['From'] = fromAddress
+    msg['To'] = toAddress
+    msg['Date'] = formatdate()
+
+    # send email
+    smtpobj.send_message(msg)
+    smtpobj.close()
+
 def main():
-    url = "https://reservation.anaihghotels.co.jp/booking/stay_pc/rsv/index.aspx?hi_id=41&lang=ja-JP&PMID=99502222&dt=2022/12/04"
+    date = '2022/12/24'
+    url = f"https://reservation.anaihghotels.co.jp/booking/stay_pc/rsv/index.aspx?hi_id=41&lang=ja-JP&PMID=99502222&dt={date}"
     
     # initialize driver
     options = webdriver.ChromeOptions()
@@ -21,9 +50,17 @@ def main():
 
     # check vacancies
     if driver.find_elements_by_xpath('//*[@id="PNoDataErr"]'):
-        print('ng')
+        text = f"""検索日: {date}
+                お探しの条件では、利用できるプラン・客室がございません。条件を変更して再度お探しください。\n
+                url: {url}"""
+        
     else:
-        print('ok')
+        num = driver.find_element_by_xpath('//*[@id="PnlPlanList"]/div[2]/p/span').text
+        text = f"""検索日: {date}
+                お探しの条件で{num}件のプランが見つかりました。\n
+                url: {url}"""
+    
+    send_email(date, text)
     
     driver.quit()
 
